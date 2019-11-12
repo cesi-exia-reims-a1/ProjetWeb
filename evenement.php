2895d6788069e0cbcc376bec4e3ae4d04fe2031d
Elem3ntary gamer <pierre.leguidevais@viacesi.fr>
2019-11-12 11:41:49 +0100

    événement quasi finis

--- evenement.php
+++ evenement.php
@@ -60,8 +60,105 @@
         			<!-- Modal -->
 				</nav>
 			</header>
+
+
 		<main>
-	
+
+		<div class="row">
+
+
+		<!--Conditions d'inscription-->
+
+
+			<div class="card text-white bg-primary mb-3" style="max-width: 18rem; margin-left:1%">
+  				<div class="card-header">Conditions d'inscription</div>
+  				<div class="card-body">
+    				<h5 class="card-title">Primary card title</h5>
+    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  				</div>
+			</div>
+
+
+			<!--Carousel événements-->
+
+
+			<div class="bd-example container">
+  				<div id="carouselExampleCaptions" class="carousel slide container" data-ride="carousel">
+    				<ol class="carousel-indicators">
+      				<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      				<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    				</ol>
+    				<div class="carousel-inner">
+      				<div class="carousel-item active">
+        				<img src="https://i.ytimg.com/vi/2VcUHKj2Nj4/maxresdefault.jpg" class="d-block w-100" alt="PGW">
+        				<div class="carousel-caption d-none d-md-block">
+          				<h5>Paris Games Week</h5>
+          				<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        				</div>
+      				</div>
+      				<div class="carousel-item">
+        				<img src="https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2019/08/gamescom-2019.jpg" class="d-block w-100" alt="Gamescom">
+        				<div class="carousel-caption d-none d-md-block">
+          				<h5>Gamescom</h5>
+          				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        				</div>
+      				</div>
+      				<div class="carousel-item">
+        				<img src="https://lemagjeuxhightech.com/wp-content/uploads/2018/06/E3-2018-demandez-le-programme-des-conferences-.jpg" class="d-block w-100" alt="E3">
+        				<div class="carousel-caption d-none d-md-block">
+          				<h5>E3</h5>
+          				<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        				</div>
+      				</div>
+    				</div>
+    				<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      				<span class="sr-only">Previous</span>
+    				</a>
+    				<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+      				<span class="sr-only">Next</span>
+    				</a>
+  				</div>
+			</div>	
+		</div>
+
+
+		<!-- Anciens événements -->
+
+
+		<div style="text-align: center; margin-top:5em;"> <h3> Les trois derniers événements !</p> </h3>
+
+		<div class="card-deck" style="margin-right:1em;margin-left:1em;">
+  			<div class="card">
+    			<img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
+    			<div class="card-body">
+      			<h5 class="card-title">titre 1</h5>
+      			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    			</div>
+			</div>
+			  
+  			<div class="card">
+    			<img class="card-img-top" src="https://images.wallpaperscraft.com/image/road_night_light_125999_1920x1080.jpg" alt="photo 2">
+    			<div class="card-body">
+      			<h5 class="card-title">titre 2</h5>
+      			<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    			</div>
+			</div>
+			  
+  			<div class="card">
+    			<img class="card-img-top" src="https://images8.alphacoders.com/926/thumb-1920-926492.jpg" alt="titre 3">
+    			<div class="card-body">
+      			<h5 class="card-title">titre 3 </h5>
+      			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+				</div>
+  			</div>
+		</div>
 		</main>
 		<footer>
 			
