a085be85edb6cb65e7fd75b6a7775722a91ee92b
vivien touvier <vivintouvier@gmail.com>
2019-11-12 16:53:29 +0100

    ajax jquery

--- apirest/ajax.js
+++ apirest/ajax.js
@@ -1,18 +1,13 @@
 // Exécute un appel AJAX GET
-// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
-function ajaxGet(url, callback) {
-    var req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            // Appelle la fonction callback en lui passant la réponse de la requête
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send(null);
-}
+
+var $articles = $ ('#articles');
+
+$.ajax({
+    type: 'GET' ,
+    url: 'http://localhost:3000/produit',
+    success: function(produits) {
+        $.each(produits, function(i, produit) {
+            $produits.append('<img src="produit.Photo_produit">')
+        })
+    }
+})
