96324c043e35d9b5e8a327693e75765c98273ddd
vivien touvier <vivintouvier@gmail.com>
2019-11-12 22:37:20 +0100

    Jquery Evenement

--- apirest/ajax.js
+++ apirest/ajax.js
@@ -2,6 +2,7 @@
 
 $(function (){
     var $articles = $('#articles');
+    var $evenement = $('#evenement');
 
     $.ajax({
         type: 'GET' ,
@@ -9,6 +10,16 @@ $(function (){
         success: function(articles) {
             $.each(articles, function(i, article) {
                 $articles.append('<img src=' + article.Photo_Produit + '>');
+            });
+        }
+    });
+
+    $.ajax({
+        type: 'GET' ,
+        url: 'http://localhost:3000/evenement',
+        success: function(evenements) {
+            $.each(evenements, function(i, evenement) {
+                $evenement.append('<img src=' + evenement.URL_PHoto + ' class="d-block w-100" alt="PGW">');
             });
         }
     });
--- evenement.php
+++ evenement.php
@@ -1,3 +1,11 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -93,13 +101,12 @@
       				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
     				</ol>
     				<div class="carousel-inner">
-      				<div class="carousel-item active">
-        				<img src="" class="d-block w-100" alt="PGW">
+      				<div class="carousel-item active" id="evenement">
         				<div class="carousel-caption d-none d-md-block">
           				<h5>Paris Games Week</h5>
         				</div>
       				</div>
-      				<div class="carousel-item" id="evenement">
+      				<div class="carousel-item">
         				<img src="https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2019/08/gamescom-2019.jpg" class="d-block w-100" alt="Gamescom">
         				<div class="carousel-caption d-none d-md-block">
           				<h5>Gamescom</h5>
@@ -163,6 +170,8 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="./vendor/node_modules/jquery/dist/jquery.js"></script>
+	<script src="./apirest/ajax.js"></script>
 
 </body>
 </html>
