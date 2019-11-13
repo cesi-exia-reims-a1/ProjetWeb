cc37d04b1a0d00cfe63cb7a7ca2d23a1c5098b44
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-11-13 22:08:41 +0100

    Rectif boutique.php

--- displayProduct.php
+++ displayProduct.php
@@ -4,11 +4,16 @@ $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '')
 
     $requete = $bdd->prepare("SELECT * FROM produit");
     $requete->execute();  
-
-while($ligne=$requete->fetch()){
-
 ?>
-<div class="container-fluid col-3 border border-secondary m-5"><center><img src=<?php echo $ligne['Photo_Produit']?>><center> <h2> <?php echo $ligne['Nom_Produit']?> </h2><h3> <?php echo  $ligne['Prix_Produit']?> €</h3></div>
-<?php
-}
-?>
+
+		<div class="container-fluid">
+			<div class="row">
+				<?php while($ligne=$requete->fetch()){ ?>
+					<div>
+						<div class="border border-secondary text-center m-5"><center><a href="produit.php"><img src=<?php echo $ligne['Photo_Produit']?>></a></center> <h2> <?php echo 	$ligne['Nom_Produit']?> </h2><h3> <?php echo  $ligne['Prix_Produit']?> €</h3>
+						</div>	
+					</div>
+				<?php
+				} ?>
+			</div>
+		</div>
