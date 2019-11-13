8abf6e8ab30e04b1f085382f0ad6883b30f94c28
Corentin Negrier <corentin.negrierpviacesi.fr>
2019-11-13 14:08:58 +0100

    cgu

--- inscription.php
+++ inscription.php
@@ -49,7 +49,7 @@
          <option selected=""> -- Choisir votre centre --</option>
        </select>
      </div>
-     
+
      <!-- création Bouton adresse mail -->
 
      <div id =register class="form-group">
@@ -65,17 +65,16 @@
       <small id="emailHelp" class="form-text text-muted">Mettre au minimum une majuscule et un chiffre !</small>
     </div>
 
-    <div id="bloc_diparition"class="form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+
+      <input type="checkbox" name="cgu" class="form-check-input" id="exampleCheck1" required="required">
       <label class="form-check-label" for="exampleCheck1">Cliquer ici pour accepter les <a href="mentions légales.html">mentions légales</a> </label>
-    </div>
+ 
 
     <!-- Bouton pour créer son compte-->
-    <div id="bloc_apparaitre">    
       <p class="signin button"> 
-      <input type="submit" value="S'inscrire"/> 
+       <input type="submit" value="S'inscrire" id="submitButton"  />
     </p>
-  </div>
+
 
   </form>
 </main> 
@@ -87,16 +86,6 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script>function affiche_bloc(CheckBox) {
-        if (CheckBox.checked) 
-        {
-            document.getElementById("block_apparaitre").style.display="block";
-        }
-        else
-        {
-            document.getElementById("bloc_disparaitre").style.display="none";
-        }
-}</script>
 <script src="./vendor/node_modules/jquery/dist/jquery.js"></script>
 <script src="./apirest/ajax.js"></script>
 </div>
--- scriptInscription.php
+++ scriptInscription.php
@@ -4,7 +4,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '')
  // Récupération des données utilisateurs 
 $Nom_Personne = (isset($_POST['Nom_Personne'])) ? ($_POST['Nom_Personne']) : NULL;  $Prenom_Personne = (isset($_POST['Prenom_Personne'])) ? ($_POST['Prenom_Personne']) : NULL;
 $Adresse_eMail =(isset($_POST['Adresse_eMail'])) ? ($_POST['Adresse_eMail']) : NULL; $Mot_De_Passe = (isset($_POST['Mot_De_Passe'])) ? ($_POST['Mot_De_Passe']) : NULL;
-$ID_Centre = (isset($_POST['ID_Centre'])) ? ($_POST['ID_Centre']) : NULL;
+$ID_Centre = (isset($_POST['ID_Centre'])) ? ($_POST['ID_Centre']) : NULL; $cgu =(isset($_POST['cgu'])) ? ($_POST['cgu']) : NULL;
  // Requête préparée pour empêcher les injections SQL 
 $requete = $bdd->prepare("SELECT Adresse_eMail FROM personne WHERE Adresse_eMail =:Adresse_eMail "); 
  // Liaison des variables de la requête préparée aux variables PHP 
@@ -28,7 +28,7 @@ else
 
 	// Requête préparée pour empêcher les injections SQL 
 	if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}$#', $Mot_De_Passe)){
-		$requete = $bdd->prepare("INSERT INTO personne ( Nom_Personne, Prenom_Personne, Adresse_eMail, Mot_De_Passe, ID_Centre) VALUES ( :Nom_Personne, :Prenom_Personne, :Adresse_eMail, :Mot_De_Passe, :ID_Centre)"); 
+		$requete = $bdd->prepare("INSERT INTO personne ( Nom_Personne, Prenom_Personne, Adresse_eMail, Mot_De_Passe, ID_Centre, cgu) VALUES ( :Nom_Personne, :Prenom_Personne, :Adresse_eMail, :Mot_De_Passe, :ID_Centre, :cgu)"); 
 	 // Liaison des variables de la requête préparée aux variables PHP 
 		$crypt = password_hash($Mot_De_Passe, PASSWORD_DEFAULT);
 		$requete->bindValue(':Nom_Personne', $Nom_Personne, PDO::PARAM_STR); 
@@ -36,6 +36,7 @@ else
 		$requete->bindValue(':Adresse_eMail', $Adresse_eMail, PDO::PARAM_STR);
 		$requete->bindValue(':Mot_De_Passe', $crypt, PDO::PARAM_STR);
 		$requete->bindValue(':ID_Centre', $ID_Centre, PDO::PARAM_STR);
+		$requete->bindValue(':cgu', $cgu, PDO::PARAM_STR);
 	// Exécution de la requête 
 		$requete->execute();
 		header('Location: index.php');
