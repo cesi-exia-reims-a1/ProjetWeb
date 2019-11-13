d0d1026d75734df2c19104825189ac6d5e064fe6
vivien touvier <vivintouvier@gmail.com>
2019-11-13 21:41:08 +0100

    displayproduct

--- displayProduct.php
+++ displayProduct.php
@@ -3,7 +3,12 @@
 $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
 
     $requete = $bdd->prepare("SELECT * FROM produit");
-    $requete->execute();
-    $ligne=$requete->fetch()
+    $requete->execute();  
 
+while($ligne=$requete->fetch()){
+
+?>
+<div class="container-fluid col-3 border border-secondary m-5"><center><img src=<?php echo $ligne['Photo_Produit']?>><center> <h2> <?php echo $ligne['Nom_Produit']?> </h2><h3> <?php echo  $ligne['Prix_Produit']?> €</h3></div>
+<?php
+}
 ?>
