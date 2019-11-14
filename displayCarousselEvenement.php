e4f6ccb85aa971fc79240879121f4853a94c7234
Elem3ntary gamer <pierre.leguidevais@viacesi.fr>
2019-11-14 14:22:11 +0100

    changement displayCarousselEvenement evenement et infoEvenement

--- displayCarousselEvenement.php
+++ displayCarousselEvenement.php
@@ -3,15 +3,17 @@
 $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
 
 $requete = $bdd->prepare("SELECT * FROM evenement WHERE Date_Evenement > (Select Date( NOW())) LIMIT 3");
-$requete->execute();
+$requete->execute();?>
    
 
-while($ligne=$requete->fetch()){
-
-?>
-
-<div class="carousel-item active"><img src= <?php echo $ligne["URL_PHoto"] ?> class="d-block w-100" alt="Evenement"> <div class="carousel-caption d-none d-md-block"> <h5><?php echo $ligne["Nom_Evenement"] ?></h5></div></div>
-<?php
-}
-
-?>
+    <?php while($ligne=$requete->fetch()){ ?>
+        <div class="card">
+            <a class="nolink" href="infoEvenement.php?id_evenement=<?php echo $ligne['ID_Evenement']?>">
+            <img class="card-img-top" src=<?php echo $ligne['URL_PHoto']?> alt="photo1">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $ligne['Nom_Evenement']?></h5>
+                <p class="card-text"><?php echo $ligne['Description_Evenement']?></p>
+            </div>
+        </div>
+    <?php
+    } ?>
--- evenement.php
+++ evenement.php
@@ -21,64 +21,14 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 
 		<main>
+		<div style="text-align: center; margin-top:1em;"> <h3> Tous nos futurs événements !</h3></div>
 
-		<div class="row" style="margin-top:2em;">
-
-
-		<!--Conditions d'inscription-->
-
-
-			<div class="card border-dark mb-3" style="max-width: 18rem; margin-left:11%">
-  				<div class="card-header">Conditions d'inscription</div>
-  				<div class="card-body">
-    				<h5 class="card-title">Primary card title</h5>
-    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  				</div>
-			</div>
-
-			<!--Carousel événements-->
-
-
-			<div class="bd-example container">
-  				<div id="carouselExampleCaptions" class="carousel slide container" data-ride="carousel">
-    				<ol class="carousel-indicators">
-      				<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-      				<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-    				</ol>
-    				<div class="carousel-inner">
-        				<?php include("displayCarousselEvenement.php"); ?>	
-      				<div class="carousel-item">
-        				<img src="https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2019/08/gamescom-2019.jpg" class="d-block w-100" alt="Gamescom">
-        				<div class="carousel-caption d-none d-md-block">
-          				<h5>Gamescom</h5>
-        				</div>
-      				</div>
-      				<div class="carousel-item">
-        				<img src="https://lemagjeuxhightech.com/wp-content/uploads/2018/06/E3-2018-demandez-le-programme-des-conferences-.jpg" class="d-block w-100" alt="E3">
-        				<div class="carousel-caption d-none d-md-block">
-          				<h5>E3</h5>
-        				</div>
-      				</div>
-    				</div>
-    				<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-      				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      				<span class="sr-only">Previous</span>
-    				</a>
-    				<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-      				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-      				<span class="sr-only">Next</span>
-    				</a>
-  				</div>
-			</div>	
+		<div class="card-deck" style="margin-right:1em;margin-left:1em;">
+			<?php include('displayCarousselEvenement.php')?>
 		</div>
 
 		<!-- Anciens événements -->
-		<div style="text-align: center; margin-top:5em;"> <h3> Les trois derniers événements !</h3></div>
+		<div style="text-align: center; margin-top:5em;"> <h3> Tous nos anciens événements !</h3></div>
 
 		<div class="card-deck" style="margin-right:1em;margin-left:1em;">
 			<?php include('displayAncienEvenement.php')?>
@@ -92,6 +42,6 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="asets/js/javascript.js"></script>
+  <script type="text/javascript" src="assets/js/javascript.js"></script>
 </body>
 </html>
