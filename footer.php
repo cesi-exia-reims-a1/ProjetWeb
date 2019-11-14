eb457d374608c25c13bd6cb947764f892445a6b7
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-11-14 17:32:29 +0100

    rectif footer

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 		<div class="container-fluid text-center">
 			<div class="row p-5 text-white">
 				<div class="col">
-					<a href="mentionslégales.php">Mention légales</a>   
+					<a class="footerText" href="mentionslégales.php"> Mention légales</a>   
 				</div>
 				<div class="col">
-					<a href="https://twitter.com/cesiingenieurs?lang=fr">Twitter</a>
+					<a class="footerText" href="https://twitter.com/cesiingenieurs?lang=fr"><img class="logo2" src="assets/image/logoTwitter"> Twitter</a>
 				</div>
 				<div class="col">
-					<a href="https://www.facebook.com/CESIingenieurs/">Facebook</a>
+					<a class="footerText" href="https://www.facebook.com/CESIingenieurs/"><img class="logo" src="assets/image/logoFacebook.png">Facebook</a>
 				</div>
 				<div class="col">
-					<a href="https://www.cesi.fr/">Site Principale du cesi</a>
+					<a class="footerText" href="https://www.cesi.fr/"><img class="logo3" src="assets/image/logoCesi.png"> Site Principale du cesi</a>
 				</div>
 			</div>
 		</div>
