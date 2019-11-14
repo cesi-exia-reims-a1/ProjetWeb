78966d0afa22c54bc89efacc29b410942e430f97
Corentin Negrier <corentin.negrierpviacesi.fr>
2019-11-14 19:03:39 +0100

    modifs a check

---
+++ ajoutPanier.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+$idPersonne = $_SESSION['id'];
+$idProduct = $_GET['id_produit'];
+$nombre = $_POST['Nombre_Article'];
+
+$bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
+
+$requete = $bdd->prepare("INSERT INTO panier VALUES (:id_produit, :id_personne, :nombre_article)");
+$requete->bindValue(':id_produit', $idProduct, PDO::PARAM_STR);
+$requete->bindValue(':id_personne', $idPersonne, PDO::PARAM_STR);
+$requete->bindValue(':nombre_article', $nombre, PDO::PARAM_STR);
+$requete->execute();  
+
+header('Location : boutique.php');
+exit();
+?>
--- infoEvenement.php
+++ infoEvenement.php
@@ -13,28 +13,30 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 	<title>BDE-Site officiel</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href ="assets/css/style.css">
+	<link rel="stylesheet" href ="assets/css/style.css">
 </head>
 <body id="couleur">
 	<header>
 		<?php include 'navbar.php' ?>
 	</header>
-			<main>
-			<?php include('displayInfoEvent.php')?>
-			<div class="container">
-				<div class="card mt-4">
-          			<img class="card-img-top img-fluid" src=<?php echo $ligne['URL_PHoto'] ?> alt="événement">
-          		<div class="card-body">
-            		<h3 class="card-title"><?php echo $ligne['Nom_Evenement']?> </h3>
-            		<h4><?php echo $ligne['Prix']?> €</h4>
-            		<p class="card-text"><?php echo $ligne['Description_Evenement']?></p>
-					<p class="signin button"> 
-       					<input type="submit" value="S'inscrire à cet événement !" id="submitButton" />
-    				</p>  
-				</div>
-				<?php include('displayPhoto.php')?>
-        		</div>
-			</div>	 	
+	<main>
+		<?php include('displayInfoEvent.php')?>
+		<div class="container">
+			<div class="card mt-4">
+				<img class="card-img-top img-fluid" src=<?php echo $ligne['URL_PHoto'] ?> alt="événement">
+				<div class="card-body">
+					<h3 class="card-title"><?php echo $ligne['Nom_Evenement']?> </h3>
+					<h4><?php echo $ligne['Prix']?> €</h4>
+					<p class="card-text"><?php echo $ligne['Description_Evenement']?></p>
+					<form action="scriptinscriptionevenement.php?id_evenement" method="post">
+						<div class="col" id="evenement">
+							<div class="m-5">
+								<input class="p-3" type="submit" value="S'inscrire à cet événement !"/>
+							</div>
+						</div>
+						<?php include('displayPhoto.php')?>
+					</div>
+				</div>	 	
 			</main>
 			<footer class="mt-3">
 				<?php include 'footer.php' ?>
@@ -45,5 +47,5 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 			<script type="text/javascript" src="asets/js/javascript.js"></script>
 
-</body>
-</html>
+		</body>
+		</html>
