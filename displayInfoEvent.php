02acadab23c8b6866de87b9379517a14a77a7226
vivien touvier <vivintouvier@gmail.com>
2019-11-14 13:34:01 +0100

    Correction

--- displayInfoEvent.php
+++ displayInfoEvent.php
@@ -1,11 +1,11 @@
 <?php
 
-$idEvent = $_GET['id_evenenment'];
+$idEvent = $_GET['id_evenement'];
 
 $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
 
     $requete = $bdd->prepare("SELECT * FROM evenement WHERE ID_Evenement = :id_evenement");
-    $requete->bindValue(':id_evenementt', $idEvent, PDO::PARAM_STR);
+    $requete->bindValue(':id_evenement', $idEvent, PDO::PARAM_STR);
     $requete->execute();  
     $ligne=$requete->fetch();
 ?>
