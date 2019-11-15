f7fa88f6dbcd9b5efe2ffc720cd62b1730deb71a
Elem3ntary gamer <pierre.leguidevais@viacesi.fr>
2019-11-15 09:47:43 +0100

    changement infoEvenement galerie et evenement

--- galerie.php
+++ galerie.php
@@ -20,7 +20,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
     <?php include 'navbar.php' ?>
 	</header>
 		<main>
-		<div class="album py-5">
+		<div class=" py-5">
         <div class="row">
             <div class="col-md-3">
               <div class="card mb-3">
@@ -69,106 +69,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
                   </div>
                 </div>
               </div>
-            </div>
-
-            <div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-			<div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-			</div>
-			
-            <div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-			</div>
-			<div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-			</div>
-			<div class="col-md-3">
-              <div class="card mb-3">
-			  <img class="card-img-top" src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" alt="photo1">
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">voir plus</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-      </div>
+            </div>          
 		</main>
 		<footer class="mt-3">
 			<?php include 'footer.php' ?>
--- infoEvenement.php
+++ infoEvenement.php
@@ -9,12 +9,15 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>BDE-Site officiel</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href ="assets/css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
 	<header>
 		<?php include 'navbar.php' ?>
@@ -28,24 +31,40 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 					<h3 class="card-title"><?php echo $ligne['Nom_Evenement']?> </h3>
 					<h4><?php echo $ligne['Prix']?> €</h4>
 					<p class="card-text"><?php echo $ligne['Description_Evenement']?></p>
-					<form action="scriptinscriptionevenement.php?id_evenement=<?php echo $ligne['ID_Evenement']?>" method="post">
+					<form action="scriptinscriptionevenement.php?id_evenement=<?php echo $ligne['ID_Evenement']?>"
+						method="post">
 						<div class="col" id="evenement">
 							<div class="m-5">
-								<input class="p-3" type="submit" value="S'inscrire à cet événement !"/>
+								<input class="p-3" type="submit" value="S'inscrire à cet événement !" />
 							</div>
 						</div>
-						<?php include('displayPhoto.php')?>
-					</div>
-				</div>	 	
-			</main>
-			<footer class="mt-3">
-				<?php include 'footer.php' ?>
-			</footer>
-			<script src="http://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-			<script type="text/javascript" src="asets/js/javascript.js"></script>
+				</div>
+			</div>
+		</div>
+		<div class="row container-fluid" style="margin-top:2em;">
+			<div class="col-md-3">
+				<div class="card mb-3">
+					<?php include('displayPhoto.php')?>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer class="mt-3">
+		<?php include 'footer.php' ?>
+	</footer>
+	<script src="http://code.jquery.com/jquery-3.4.1.min.js"
+		integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+	</script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+	</script>
+	<script type="text/javascript" src="asets/js/javascript.js"></script>
 
-		</body>
-		</html>
+</body>
+
+</html>
