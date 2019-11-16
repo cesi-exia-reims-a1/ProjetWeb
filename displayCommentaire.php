34bafa3cca2ca93aed6ec952ece1b51b3d7d23ed
vivien touvier <vivintouvier@gmail.com>
2019-11-16 17:20:27 +0100

    script signalement commentaire

--- displayCommentaire.php
+++ displayCommentaire.php
@@ -10,7 +10,16 @@ $requete->execute();
 while($ligne=$requete->fetch()){ ?>
     <div class="card mb-2">
         <div class="container card">
-            <p><?php echo $ligne['Texte']?></p>           
+            <p><?php echo $ligne['Texte']?></p>   
+            <?php if(isset($_SESSION['status']) && $_SESSION['status'] == 1){ ?>
+			<form method="post" action="scriptSignalementCommentaire.php">
+				<p class="signin button mt-1 mb-1 ml-3"> 
+				<input type="hidden" value="<?php echo $ligne['ID_Commentaire'] ?>" name="id_com">
+	        	<input type="submit" value="Signaler" name="Signaler" id="submitButton"  />
+	   		 	</p>
+			</form>
+			<?php  
+			} ?>        
         </div>
     </div>
 <?php
--- scriptSignalementCommentaire.php
+++ scriptSignalementCommentaire.php
@@ -8,17 +8,17 @@ $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '')
  // Récupération des données utilisateurs 
 
 $Signale =$_POST['Signaler'];
-$idEvent =$_POST['id_event'];
+$idCom =$_POST['id_com'];
 
  // Requête préparée pour empêcher les injections SQL 
-$requete = $bdd->prepare("UPDATE evenement SET Signale = 1  WHERE :Signale = 'Signaler' AND ID_Evenement = :id_evenement ");
+$requete = $bdd->prepare("UPDATE commentaire SET Signale = 1  WHERE :Signale = 'Signaler' AND ID_Commentaire = :id_com ");
 $requete->bindValue(':Signale', $Signale, PDO::PARAM_STR);
-$requete->bindValue(':id_evenement', $idEvent, PDO::PARAM_STR);
+$requete->bindValue(':id_com', $idCom, PDO::PARAM_STR);
 	// Exécution de la requête 
 $requete->execute();
 $requete->closecursor();
  
-mail($Destinataires, "Signalement evenement", "L'évenement $idEvent a été signalé !\n PS : Romain t'es un fdp");
+mail($Destinataires, "Signalement commentaire", "Le commentaire $idCom a été signalé !\n PS : Romain t'es un fdp");
 
 header("location:".  $_SERVER['HTTP_REFERER']); 
 exit();
