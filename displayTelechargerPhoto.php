370b79514957d9ff333fc42763598add705d3f26
Elem3ntary gamer <pierre.leguidevais@viacesi.fr>
2019-11-16 15:18:36 +0100

    j'arrive pas a télécharger les photos j'abandonne

---
+++ displayTelechargerPhoto.php
@@ -0,0 +1,7 @@
+<?php
+
+	if(isset($_SESSION['id']) && $_SESSION['id'] == 1){ ?>
+		<div class="bd-highlight" /><a href="admin.php" class="nav-item nav-link active text-danger">Télécharger toutes les photos !</a></div>
+	<?php  
+	} 
+	?>
