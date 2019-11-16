290dd417bb049315d69b3369e8c4b8174bb0cc55
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-11-16 17:56:44 +0100

    Rectif signalement photo

--- scriptsignalementphotos.php
+++ scriptsignalementphotos.php
@@ -10,7 +10,7 @@ $Signale =$_POST['Signale'];
 $idPhoto = $_POST['id_photo'];
 
  // Requête préparée pour empêcher les injections SQL 
-$requete = $bdd->prepare("UPDATE photo SET Signale = 1  WHERE :Signale = 'Signale' AND ID_Photo = :id_photo");
+$requete = $bdd->prepare("UPDATE photo SET Signale = 1  WHERE :Signale = 'Signaler' AND ID_Photo = :id_photo");
 $requete->bindValue(':Signale', $Signale, PDO::PARAM_STR);
 $requete->bindValue(':id_photo', $idPhoto, PDO::PARAM_STR);
 
