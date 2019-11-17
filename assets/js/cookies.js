43ce254c32a25d79d8f9295e6d79c0b6b0c30769
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-11-17 13:15:02 +0100

    cookies

---
+++ assets/js/cookies.js
@@ -0,0 +1,16 @@
+window.cookieconsent.initialise({
+		"palette": {
+			"popup": {
+				"background": "#252e39"
+			},
+			"button": {
+				"background": "#14a7d0"
+			}
+		},
+		"position": "bottom-right",
+		"content": {
+		"message": "Ce site utilise des cookies pour vous garantir la meilleure expérience sur notre site.",
+		"dismiss": "J'ai compris et J'accepte",
+		"link": "En savoir plus"
+	}
+});
