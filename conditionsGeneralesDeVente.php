2a5e258dbbd2d27da0fed09daca2bd1921e44944
Elem3ntary gamer <pierre.leguidevais@viacesi.fr>
2019-11-18 15:52:27 +0100

    changmeent pour le SEO

--- conditionsGeneralesDeVente.php
+++ conditionsGeneralesDeVente.php
@@ -11,11 +11,16 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <title> Conditions générales de vente</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
+	<title>BDE-Site officiel</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta name="description" content="Site du BDE, ici vous pourrez acheté des produits en fonction de votre centre, participer à des évenements, poster, liker et commenter des photos" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css"
+		href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
+
 </head>
 <header>
     <?php include 'navbar.php' ?>
