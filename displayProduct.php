8be5df8d1bfde0081e89d25d0b43b9c4ec60709d
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-11-18 08:55:13 +0100

    rectif displayProduct

--- displayProduct.php
+++ displayProduct.php
@@ -3,6 +3,8 @@
 //CEST MAL IL FAUT UN FICHIER bdd.php et include ça partout plutot que c/c cette ligne
 $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
 
+$idCentre = $_SESSION['centre'];
+
 if(isset($_POST['categories']) && $_POST['categories'] != 'no-cat' && isset($_POST['prix']) && $_POST['prix'] == 'asc'){//si on a un prix croissant + un filtre de catégorie
 	$requete = $bdd->prepare("SELECT * FROM produit WHERE Categorie_Produit = :categorie AND Prix_Produit > :prixAsc AND Supprime = 0 AND ID_Centre = :id_centre ORDER BY Prix_Produit ASC");
 	$requete->bindParam('categorie', $_POST['categories'], PDO::PARAM_STR);
@@ -18,16 +20,15 @@ else if(isset($_POST['categories']) && $_POST['categories'] != 'no-cat' && isset
 	$requete->execute();
 }
 else if(isset($_POST['prix']) && $_POST['prix'] == 'asc'){//si on a que un filtre de prix croissant
-	$requete = $bdd->prepare("SELECT * FROM produit WHERE Supprime = 0 AND ID_Centre = :id_centre ORDER BY Prix_Produit ASC");
-	$requete->bindParam('id_centre', $idCentre, PDO::PARAM_STR);
+	$requete = $bdd->prepare("SELECT * FROM produit WHERE Supprime = 0 AND Prix_Produit > :prixAsc AND ID_Centre = :id_centre ORDER BY Prix_Produit ASC");
 	$requete->bindParam('prixAsc', $_POST['prix'], PDO::PARAM_STR);
+	$requete->bindParam('id_centre', $idCentre, PDO::PARAM_STR);
 	$requete->execute();
 }
 else if(isset($_POST['prix']) && $_POST['prix'] == 'desc'){//si on a que un filtre de prix décroissant
-	$requete = $bdd->prepare("SELECT * FROM produit WHERE Supprime = 0 AND ID_Centre = :id_centre ORDER BY Prix_Produit DESC");
-	$requete->bindParam('id_centre', $idCentre, PDO::PARAM_STR);
+	$requete = $bdd->prepare("SELECT * FROM produit WHERE Supprime = 0 AND Prix_Produit > :prixDesc AND ID_Centre = :id_centre ORDER BY Prix_Produit DESC");
 	$requete->bindParam('prixDesc', $_POST['prix'], PDO::PARAM_STR);
-	$requete->execute();
+	$requete->bindParam('id_centre', $idCentre, PDO::PARAM_STR);
 	$requete->execute();
 }
 else if(isset($_POST['categories']) && $_POST['categories'] != 'no-cat'){//Si on a que un filtre de catégorie
