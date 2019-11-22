720d0e029137813ca689044a7021c6afe7415f60
vivien touvier <vivintouvier@gmail.com>
2019-11-22 13:53:44 +0100

    Bouton déjà inscrit

--- displayInscription.php
+++ displayInscription.php
@@ -1,12 +1,26 @@
 <?php
 $today = date("Y-m-d");
+$idPers = $_SESSION['id'];
+$idEvent = $_GET['id_evenement'];
+include('bdd.php');
+$requete = $bdd->prepare("SELECT * FROM participer WHERE ID_Evenement = :id_evenement AND ID_Personne = :id_personne");
+$requete->bindValue(':id_evenement', $idEvent, PDO::PARAM_STR);
+$requete->bindValue(':id_personne', $idPers, PDO::PARAM_STR);
+ 
 if($dateEvent>$today && isset($_SESSION['pseudo'])){
-    echo "<form action='scriptinscriptionevenement.php?id_evenement="?><?php echo $ligne['ID_Evenement']?><?php echo"'
-    method='post'>
-    <div class='col' id='evenement'>
-        <div class='m-5'>
+    $requete->execute(); 
+    if($requete->rowCount() > 0){?>
+        <input type="button" disabled="disabled" value="Vous êtes inscrits à cet évènement" class="m-5"/>
+    <?php }
+    else{?>
+        <form action='scriptinscriptionevenement.php?id_evenement=<?php echo $ligne['ID_Evenement']?>' method='post'>
+            <div class='col' id='evenement'>
+            <div class='m-5'>
             <input class='p-3' type='submit' value='S inscrire à cet événement !' />
-        </div>
-    </div>
-    </form>";
-}
+            </div>
+            </div>
+        </form>
+    <?php
+    }
+} 
+?>
